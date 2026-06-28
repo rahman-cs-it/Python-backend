b074bd5fdefc78b31248b6ae7b091df09180af45
Rahman it <siddiquewasifur786123@gmail.com>
2026-06-28 12:32:43 +0530

    added input

---
+++ UserInput.py
@@ -0,0 +1,9 @@
+name = input("Enter your name : ")
+age = input('Enter your age : ')
+print('Hello',name ,'your age is',age)
+
+sentence = input("Please enter your sentence ")
+print(sentence)
+word1 = input('please enter word ,you want to changed : ')
+word2 = input('please enter the changed word : ')
+print(sentence.replace(word1,word2))
