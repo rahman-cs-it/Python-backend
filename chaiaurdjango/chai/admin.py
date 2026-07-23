116bc31ad8e19db79ddf435735c50d1b804bf4a5
Rahman it <siddiquewasifur786123@gmail.com>
2026-07-23 19:14:52 +0530

    added models

--- chaiaurdjango/chai/admin.py
+++ chaiaurdjango/chai/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import ChaiVarity
 
+from .models import ChaiVarity,ChaiReveiw,Stores,Certificate
 # Register your models here.
-admin.site.register(ChaiVarity)
+class ChaiReviewInLine(admin.TabularInline):
+    model = ChaiReveiw
+    extra = 2;
+    
+class ChaiVarityAdmin(admin.ModelAdmin):
+    list_display = ('name','type','date_added')
+    inlines = [ChaiReviewInLine]
+    
+class StoresAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('chai_variteies',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai','certificate_number')
+    
+admin.site.register(ChaiVarity,ChaiVarityAdmin)
+admin.site.register(Stores,StoresAdmin)
+admin.site.register(Certificate,ChaiCertificateAdmin)
